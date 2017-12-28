c976d507703d644835a2e4f34520557e96c3a217
Bhavesh Vinod Oswal <oswal.bhavesh2010@gmail.com>
2017-12-28 16:53:29 +0530

    Update model.py
    updated to newer version of requirements

--- model.py
+++ model.py
@@ -1,56 +1,65 @@
-from keras.layers import Input, Dense, Embedding, merge, Convolution2D, MaxPooling2D, Dropout
-from sklearn.cross_validation import train_test_split
-from keras.layers.core import Reshape, Flatten
+from keras.layers import Input, Dense, Embedding, Conv2D, MaxPool2D
+from keras.layers import Reshape, Flatten, Dropout, Concatenate
 from keras.callbacks import ModelCheckpoint
-from data_helpers import load_data
 from keras.optimizers import Adam
 from keras.models import Model
+from sklearn.model_selection import train_test_split
+from data_helpers import load_data
 
-
-
-print 'Loading data'
+print('Loading data')
 x, y, vocabulary, vocabulary_inv = load_data()
+
+# x.shape -> (10662, 56)
+# y.shape -> (10662, 2)
+# len(vocabulary) -> 18765
+# len(vocabulary_inv) -> 18765
 
 X_train, X_test, y_train, y_test = train_test_split( x, y, test_size=0.2, random_state=42)
 
-sequence_length = x.shape[1]
-vocabulary_size = len(vocabulary_inv)
+# X_train.shape -> (8529, 56)
+# y_train.shape -> (8529, 2)
+# X_test.shape -> (2133, 56)
+# y_test.shape -> (2133, 2)
+
+
+sequence_length = x.shape[1] # 56
+vocabulary_size = len(vocabulary_inv) # 18765
 embedding_dim = 256
 filter_sizes = [3,4,5]
 num_filters = 512
 drop = 0.5
 
-nb_epoch = 100
+epochs = 100
 batch_size = 30
 
 # this returns a tensor
+print("Creating Model...")
 inputs = Input(shape=(sequence_length,), dtype='int32')
-embedding = Embedding(output_dim=embedding_dim, input_dim=vocabulary_size, input_length=sequence_length)(inputs)
+embedding = Embedding(input_dim=vocabulary_size, output_dim=embedding_dim, input_length=sequence_length)(inputs)
 reshape = Reshape((sequence_length,embedding_dim,1))(embedding)
 
-conv_0 = Convolution2D(num_filters, filter_sizes[0], embedding_dim, border_mode='valid', init='normal', activation='relu', dim_ordering='tf')(reshape)
-conv_1 = Convolution2D(num_filters, filter_sizes[1], embedding_dim, border_mode='valid', init='normal', activation='relu', dim_ordering='tf')(reshape)
-conv_2 = Convolution2D(num_filters, filter_sizes[2], embedding_dim, border_mode='valid', init='normal', activation='relu', dim_ordering='tf')(reshape)
+conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
 
-maxpool_0 = MaxPooling2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), border_mode='valid', dim_ordering='tf')(conv_0)
-maxpool_1 = MaxPooling2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), border_mode='valid', dim_ordering='tf')(conv_1)
-maxpool_2 = MaxPooling2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), border_mode='valid', dim_ordering='tf')(conv_2)
+maxpool_0 = MaxPool2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
+maxpool_1 = MaxPool2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
+maxpool_2 = MaxPool2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
 
-merged_tensor = merge([maxpool_0, maxpool_1, maxpool_2], mode='concat', concat_axis=1)
-flatten = Flatten()(merged_tensor)
-# reshape = Reshape((3*num_filters,))(merged_tensor)
+concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+flatten = Flatten()(concatenated_tensor)
 dropout = Dropout(drop)(flatten)
-output = Dense(output_dim=2, activation='softmax')(dropout)
+output = Dense(units=2, activation='softmax')(dropout)
 
 # this creates a model that includes
-model = Model(input=inputs, output=output)
+model = Model(inputs=inputs, outputs=output)
 
 checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
-adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 
 model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, callbacks=[checkpoint], validation_data=(X_test, y_test))  # starts training
-
+print("Traning Model...")
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[checkpoint], validation_data=(X_test, y_test))  # starts training
 
 
 
